49ab49d181d36ca2cc0803b0fba3094242071ca0
stillesjo <a.stillesjo@gmail.com>
2017-07-20 10:28:37 +0200

    First commit - Basic setup
    Generated the basic Atom package and made some smaller adjustments.

---
+++ spec/generate-post-folder-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GeneratePostFolderView from '../lib/generate-post-folder-view';
+
+describe('GeneratePostFolderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
