9e3554a3164f3d8f1d59b4b7cfca4e29f5b16456
stillesjo <a.stillesjo@gmail.com>
2017-07-29 08:05:02 +0200

    Implement configurable src dir for pages.
    Add field in package.json called postFolderSrcDir with a path. The path
    will be the directory where posts will be placed.

--- spec/post-folder-creator-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import PostFolderCreatorView from '../lib/post-folder-creator-view';
-
-describe('GeneratePostFolderView', () => {
-  it('has one valid test', () => {
-    // expect('life').toBe('easy');
-  });
-});
